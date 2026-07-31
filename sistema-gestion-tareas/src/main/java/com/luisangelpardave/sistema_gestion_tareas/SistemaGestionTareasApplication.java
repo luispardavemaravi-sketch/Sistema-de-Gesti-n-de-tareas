1b37ca5178ab928fcece4f7d80823fee74292c51
Luisangel <luispardavemaravi@gmail.com>
2026-07-30 20:44:37 -0500

    Initial Spring Boot commit

---
+++ sistema-gestion-tareas/src/main/java/com/luisangelpardave/sistema_gestion_tareas/SistemaGestionTareasApplication.java
@@ -0,0 +1,13 @@
+package com.luisangelpardave.sistema_gestion_tareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestionTareasApplication.class, args);
+	}
+
+}
---
+++ sistema-gestion-tareas/src/test/java/com/luisangelpardave/sistema_gestion_tareas/SistemaGestionTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisangelpardave.sistema_gestion_tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestionTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
